e731cc437c992389627b4d7a3f371ab4bbcc9489
gathani <sapna.gathani@girlswhocode.com>
2017-07-27 22:11:43 -0400

    Blog starter

---
+++ blog-starter/blog.js
@@ -0,0 +1,23 @@
+class BlogEntry {
+
+}
+
+function addEntryToBlog() {
+  //Create new blog entry
+  var blogEntry = new BlogEntry(/*pass in name and blog text as parameters*/));
+
+  //Add the new entry, name and date to the blog
+  createBlogEntryElement(blogEntry);
+  createFooterElement(blogEntry);
+
+  //Clear the inputs
+
+}
+
+function createBlogEntryElement(blogEntry) {
+
+}
+
+function createFooterElement(blogEntry) {
+
+}
